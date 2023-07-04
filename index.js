df26f4897965995844f05c27cf611d0944010ce1
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-07-04 16:35:20 -0300

    Se agregan las configuraciones basicas para cualquier backend

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-import express  from "express"
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import * as dotenv from 'dotenv';
 
-const app = express()
+dotenv.config();
+const app = express();
+
 app.set('port', process.env.PORT || 3000)
-app.listen(app.get('port'), ()=>{
-    console.log('Servidor iniciado en el port: '+ app.get('port'))
-})
+app.listen(app.get('port'), () => {
+  console.log('Servidor iniciado en el port: ' + app.get('port'))
+});
+
+//  middelwars
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // permite recibir strings y arrays en el request
+app.use(morgan('dev'));
+
+//  rutas
+
+app.get('/prueba', (_request, response) => {
+  response.send('Solicitud get al backend')
+});
