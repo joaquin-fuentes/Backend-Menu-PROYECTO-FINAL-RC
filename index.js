d410d3fa3bd3a2ae359dcaff40dcd444962762b6
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-07-04 15:40:59 -0300

    Agregamos el puerto 3000

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import express  from "express"
 
 const app = express()
+app.set('port', process.env.PORT || 3000)
+app.listen(app.get('port'), ()=>{
+    console.log('Servidor iniciado en el port: '+ app.get('port'))
+})
