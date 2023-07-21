7fbefee5174305b168435e2cc0a6f8c5b0b0befd
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-07-21 19:00:48 -0300

    Se crea una nueva ruta donde se pasa el id por parametro y se unsa el controlador borrarProducto en la peticion delete

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { crearProducto, obtenerProductos } from '../controllers/productos.controllers';
+import { borrarProducto, crearProducto, obtenerProductos } from '../controllers/productos.controllers';
 
 const router = Router();
 
 router.route('/productos').get(obtenerProductos).post(crearProducto)
+router.route('/productos/:id').delete(borrarProducto)
 
 export default router;
