76194d2b0daf87e1dacf91ce6e1505cce09ccbb0
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-07-21 17:43:46 -0300

    Se realiza la coneccion a la base de datos

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -3,7 +3,9 @@ import { MONGODB_URI } from '../config';
 
 (async () => {
   try {
-    const db = await connect(MONGODB_URI);
+    const db = await connect(MONGODB_URI, {
+      family: 4
+    });
     console.log(`DB conectada en ${db.connection.name}`);
   } catch (error) {
     console.log(error);
