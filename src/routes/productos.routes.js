482031a749374808c8879269255463b182b7b14e
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-07-21 19:28:15 -0300

    Se agrega el controlador editarProducto en la peticion PUT

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { borrarProducto, crearProducto, obtenerProductos } from '../controllers/productos.controllers';
+import { borrarProducto, crearProducto, editarProducto, obtenerProductos } from '../controllers/productos.controllers';
 
 const router = Router();
 
 router.route('/productos').get(obtenerProductos).post(crearProducto)
-router.route('/productos/:id').delete(borrarProducto)
+router.route('/productos/:id').delete(borrarProducto).put(editarProducto)
 
 export default router;
