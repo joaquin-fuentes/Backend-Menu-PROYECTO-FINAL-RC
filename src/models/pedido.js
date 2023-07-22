38b5efd385d3187db85292c180e5c89fdce169ca
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-07-22 19:04:27 -0300

    Se agrega el estado en el modelo de pedido

--- src/models/pedido.js
+++ src/models/pedido.js
@@ -15,6 +15,10 @@ const pedidoSchema = new Schema({
     type: Date,
     default: Date.now
   },
+  estado: {
+    type: Boolean,
+    required: true
+  },
   subTotal: {
     type: Number,
     required: true
