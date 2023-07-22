8992d557470c6b7785a1ce490c50397fc1d40509
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-07-22 18:14:21 -0300

    Se crea el controlador para crear los pedidos

--- src/controllers/pedidos.controllers.js
+++ src/controllers/pedidos.controllers.js
@@ -11,3 +11,18 @@ export const obtenerPedidos = async (_request, response) => {
     });
   }
 };
+
+export const crearProducto = async (request, response) => {
+  try {
+    const pedidoNuevo = new Pedido(request.body);
+    await pedidoNuevo.save();
+    response.status(201).json({
+      mensaje: "Pedido creado con exito"
+    });
+  } catch (error) {
+    console.log(error);
+    response.status(404).json({
+      mensaje: "Error al crear el pedido"
+    });
+  }
+};
