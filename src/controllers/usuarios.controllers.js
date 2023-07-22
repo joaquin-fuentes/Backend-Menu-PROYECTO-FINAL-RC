213029a8a506c51c8b318c257dddc82842060fd7
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-07-22 16:27:17 -0300

    Se agrega bcrypt y se crea el controlador para crear usuarios

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,4 +1,5 @@
 import Usuario from "../models/usuario";
+import bcrypt from 'bcrypt';
 
 export const obtenerUsuarios = async (_request, response) => {
   try {
@@ -8,6 +9,37 @@ export const obtenerUsuarios = async (_request, response) => {
     console.log(error);
     response.status(404).json({
       mensaje: "Error al buscar los usuarios"
+    });
+  }
+};
+
+export const crearUsuario = async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return response.status(400).json({
+        mensaje: "El correo ya esta registrado"
+      });
+    }
+
+    usuario = new Usuario(request.body);
+
+    const salt = bcrypt.genSaltSync(10);
+    usuario.password = bcrypt.hashSync(password, salt)
+    await usuario.save();
+
+    response.status(201).json({
+      mensaje: "usuario creado",
+      nombre: usuario.nombreUsuario,
+      uid: usuario._id
+    });
+  } catch (error) {
+    console.log(error);
+    response.status(400).json({
+      mensaje: "El usuario no pudo ser creado"
     });
   }
 };
